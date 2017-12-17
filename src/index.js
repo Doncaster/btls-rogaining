801efc6a837c589b90f273f1ccecc1e64911b7cc
Teemu Leinonen <teemu.leinonen@gmail.com>
2017-12-17 14:18:37 +0200

    Added project dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Firebase from 'firebase/app';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import firebaseConfig from '../firebase-config.js';
+
+Firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
